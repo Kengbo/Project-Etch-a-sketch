4c5bde15b4de77f4695b19473b6b28135c4e7140
kengbo <kennethgbo@gmail.com>
2025-11-20 16:31:58 +0000

    Added the other elements to complet the Project

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-let timeId = document.getElementById("timeId");
 
+const container =  document.getElementById('container');
+const clear = document.getElementById('clear');
+let alerts = document.getElementById('alert');
+const cells = 100;
+const totalCells = cells * cells;
+let paintEnabled = true;
 
+for(i = 1; i <= totalCells; i++){
 
-// timeId.textContent = `Current Time: ${hours}:${minutes}:${seconds}`;
+   const div=  document.createElement('div');
+   div.classList.add('box');
+   container.append(div);
+   }
+let changeGrid = ()=>{
 
-setInterval(() => {
-    let now = new Date();
-    let hrs = now.getHours();
-    let mins = now.getMinutes();
-    let secs = now.getSeconds();
-    timeId.textContent = `Current Time: ${hrs}:${mins}:${secs}`;
-}, 1000);
+    let size = document.getElementById('changeSize').value;
+   
+    if(size > 100){
+        size = 100;
+    }
+    if(size < 16){
+        size = 16;
+    }
+      document.documentElement.style.setProperty("--boxHeight", size);
+}
+
+const box = document.querySelectorAll('.box');
+
+    box.forEach((box) =>{
+        box.addEventListener('mouseover', ()=>{
+            if (paintEnabled){
+            box.style.backgroundColor = 'black';
+            alerts.textContent = 'Press Space Bar to Disable brush'; 
+            }
+        })
+    })
+document.addEventListener('keydown', (e)=>{
+    if(e.code === "Space"){
+        e.preventDefault();
+        paintEnabled =!paintEnabled;
+        alerts.textContent = 'Brush Disabled'; 
+    }      
+});
+clear.addEventListener('click', ()=>{
+    const boxes = document.querySelectorAll('.box');
+        boxes.forEach(b => b.style.backgroundColor = "rgb(246, 252, 252)");
+})
